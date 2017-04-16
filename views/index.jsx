83ea784ef210b5a408a6e91055ce7e4d32be67ed
Michael Macherey <macherey@gmx.net>
2017-04-16 15:02:34 +0200

    add solution PROP_AND_STATE

--- views/index.jsx
+++ views/index.jsx
@@ -5,62 +5,67 @@ export default class TodoBox extends React.Component {
         return (
             <div className="todoBox">
                 <h1>Todos</h1>
-                <TodoList data={this.props.data}/>
-                <TodoForm/>
+                <TodoList data={this.props.data} />
+                <TodoForm />
             </div>
         );
     }
 }
 
 class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: this.props.data,
-      titleValue: "",
-      detailValue: ""
-    };
-    this.changeTitle = this.changeTitle.bind(this);
-    this.changeDetail = this.changeDetail.bind(this);
-    this.addTodo = this.addTodo.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: this.props.data,
+            titleValue: "",
+            detailValue: ""
+        };
+        this.changeTitle = this.changeTitle.bind(this);
+        this.changeDetail = this.changeDetail.bind(this);
+        this.addTodo = this.addTodo.bind(this);
+        this.deleteTodo = this.deleteTodo.bind(this);
+    }
 
-  changeTitle(e) {
-    this.setState({titleValue: e.target.value});
+    changeTitle(e) {
+        this.setState({ titleValue: e.target.value });
+    }
 
-  }
+    changeDetail(e) {
+        this.setState({ detailValue: e.target.value });
+    }
 
-  changeDetail(e) {
-    this.setState({detailValue: e.target.value});
+    addTodo() {
+        let newData = this.state.data;
+        newData.push({
+            title: this.state.titleValue,
+            detail: this.state.detailValue
+        });
+        this.setState({ data: newData });
+        this.setState({ titleValue: "" });
+        this.setState({ detailValue: "" });
+    }
 
-  }
-
-  addTodo() {
-    let newData = this.state.data;
-    newData.push({
-      title: this.state.titleValue,
-      detail: this.state.detailValue
-    });
-    this.setState({data: newData});
-    this.setState({titleValue: ""});
-    this.setState({detailValue: ""});
-
-  }
+    deleteTodo(title) {
+        let newData = this.state.data.filter(function (todo) {
+            return todo.title !== title;
+        });
+        this.setState({ data: newData });
+    }
 
     render() {
         var todo = this.props.data.map(function (obj) {
-            return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>
-        });
+            return <Todo title={obj.title} key={obj.title} onDelete={this.deleteTodo}>{obj.detail}</Todo>
+        }.bind(this));
         return (
             <div className="todoList">
-              <div>
-                <span>Title:</span><input type="text" value={this.state.titleValue} onChange={this.changeTitle} />
-                <span>Detail:</span><input type="text" value={this.state.detailValue} onChange={this.changeDetail} />
-                <button onClick={this.addTodo}>Add</button>
-              </div>
-                <table style={{border: "2px solid black"}}>
+                <div>
+                    <span>Title:</span><input type="text" value={this.state.titleValue} onChange={this.changeTitle} />
+                    <span>Detail:</span><input type="text" value={this.state.detailValue} onChange={this.changeDetail} />
+                    <button onClick={this.addTodo}>Add</button>
+                </div>
+                <table style={{ border: "2px solid black" }}>
                     <tbody>
-                    {todo}
+                        {todo}
                     </tbody>
                 </table>
             </div>
@@ -76,6 +81,7 @@ class Todo extends React.Component {
             TodoStyle: style.notCheckedTodo
         };
         this.handleChange = this.handleChange.bind(this);
+        this._onDelete = this._onDelete.bind(this);
     }
 
     handleChange(e) {
@@ -93,11 +99,18 @@ class Todo extends React.Component {
         }
     }
 
+    _onDelete() {
+        this.props.onDelete(this.props.title);
+    }
+
     render() {
         return (
             <tr style={this.state.TodoStyle}>
                 <td style={style.tableContent}>
-                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+                    <button onClick={this._onDelete}>X</button>
+                </td>
+                <td style={style.tableContent}>
+                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange} />
                 </td>
                 <td style={style.tableContent}>{this.props.title}</td>
                 <td style={style.tableContent}>{this.props.children}</td>
@@ -105,6 +118,7 @@ class Todo extends React.Component {
         );
     }
 }
+
 Todo.propTypes = {
     title: React.PropTypes.string.isRequired
 };
